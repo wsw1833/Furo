b64ea319e66a4eedfcf1ef601b7aa1766e01523a
Zhan Choon <hfyzw1@nottingham.edu.my>
2025-02-28 18:56:08 +0800

    hero page

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,63 @@
-import Image from "next/image";
+'use client';
+import Image from 'next/image';
+import animals from '@images/animals.png';
+import icon from '@images/icon.png';
+import scroll from '@images/scroll.png';
+import metamask from '@images/metamask.png';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const router = useRouter();
+  function connectHandler() {
+    //metamask wallet connection (server component)
+    router.push('/profile');
+  }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div className="flex overflow-none">
+      <div className="flex flex-col w-[50%] mt-18 px-12">
+        <div className="flex flex-row items-center justify-start gap-2">
+          <Image
+            lazy="true"
+            src={icon}
+            alt="icon"
+            width={78}
+            height={78}
+          ></Image>
+          <div className="text-5xl font-semibold">Petify</div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p className="line-clamp-2 mt-10 text-6xl leading-auto font-bold leading-20">
+          Pawprints in the Blockchain
+        </p>
+        <p className="line-clamp-2 mt-8 text-4xl font-medium w-[60%] leading-12">
+          Revolutionizing Pet Care with Soulbound
+        </p>
+        <div className="flex flex-row items-center justify-start gap-2 mt-6">
+          <p className="text-base font-medium text-[#484848]">
+            Powered By Scroll.
+          </p>
+          <Image src={scroll} alt="icon" width={32} height={32}></Image>
+        </div>
+        <button
+          onClick={connectHandler}
+          className="bg-[#FFC65C] mt-16 w-[50%] h-auto py-2 font-medium rounded-[12px] text-lg flex gap-4 items-center justify-center"
         >
+          Connect With Metamask
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            lazy="true"
+            src={metamask}
+            alt="metamask logo"
+            width={32}
+            height={32}
+          ></Image>
+        </button>
+      </div>
+      <Image
+        className="w-[50%] max-h-200 md:min-h-180 md:max-w-full"
+        src={animals}
+        alt="animal picture"
+        lazy="true"
+      ></Image>
     </div>
   );
 }
