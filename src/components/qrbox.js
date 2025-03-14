9ea30bf015bffcc045ec026ea47b22fae34bad26
wsw1833 <johndoe@example.com>
2025-03-15 00:17:49 +0800

    fix QR scan bug

--- src/components/qrbox.js
+++ src/components/qrbox.js
@@ -5,7 +5,7 @@ import { useQRCode } from 'next-qrcode';
 import Link from 'next/link';
 export default function QrBox({ petId }) {
   const { Canvas } = useQRCode();
-  const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL || 'localhost:3000';
+  const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000';
   return (
     <div className="container flex flex-col w-full h-full">
       <div className="w-full flex flex-row items-center justify-start gap-2">
@@ -14,16 +14,16 @@ export default function QrBox({ petId }) {
       </div>
       <div className="w-full h-full flex items-center justify-center">
         <Canvas
-          text={`http://${baseURL}/qrscan?petId=${petId}`}
+          text={`${baseURL}/qrscan?petId=${petId}`}
           options={{
             type: 'image/jpeg',
-            scale: 5,
+            scale: 4.5,
           }}
         />
       </div>
       <div className="w-full flex flex-col items-center justify-center gap-1">
         <span className="font-semibold">OR</span>
-        <Link target="_blank" href={`http://${baseURL}/qrscan?petId=${petId}`}>
+        <Link target="_blank" href={`${baseURL}/qrscan?petId=${petId}`}>
           <span className="text-sm  underline text-[#2B87FF]">
             Click here to navigate
           </span>
